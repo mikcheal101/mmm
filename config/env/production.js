444ab43e8a2f458b55966ec4ab7581876c2b537b
hirekaanmicheal@gmail.com <hirekaanmicheal@gmail.com>
2017-02-26 12:38:26 +0100

    heroku

--- config/env/production.js
+++ config/env/production.js
@@ -12,7 +12,7 @@
 
 module.exports = {
 
-    hookTimeout: 120000,
+
     /***************************************************************************
     * Set the default database connection for models in the production        *
     * environment (see config/connections.js and config/models.js )           *
@@ -21,19 +21,5 @@ module.exports = {
     models: {
         connection: 'heroku_settings'
     },
-
-  /***************************************************************************
-   * Set the port in the production environment to 80                        *
-   ***************************************************************************/
-
-  // port: 80,
-
-  /***************************************************************************
-   * Set the log level in production environment to "silent"                 *
-   ***************************************************************************/
-
-  // log: {
-  //   level: "silent"
-  // }
-
+    hookTimeout: 120000
 };
